54d273f0316ba025ad61e4be287a40e18a555543
Thomas Nemeth <thonem10@gmail.com>
2022-04-13 17:32:10 +0200

    crup

--- app/src/main/java/no/kristiania/android/reverseimagesearchapp/presentation/fragment/UploadImageFragment.kt
+++ app/src/main/java/no/kristiania/android/reverseimagesearchapp/presentation/fragment/UploadImageFragment.kt
@@ -1,5 +1,7 @@
 package no.kristiania.android.reverseimagesearchapp.presentation.fragment
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
@@ -7,9 +9,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.edmodo.cropper.CropImageView
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import no.kristiania.android.reverseimagesearchapp.R
@@ -27,8 +31,9 @@ class UploadImageFragment : Fragment(R.layout.fragment_upload_image){
     private lateinit var chooseImageBtn: Button
     private lateinit var selectedImage: UploadedImage
     private lateinit var captureImageBtn: Button
-    private lateinit var photoView: ImageView
-    private lateinit var cropFragmentBtn : Button
+    private lateinit var cropImageView: CropImageView
+
+
 
     //ViewModels need to be instantiated after onAttach()
     //So we do not inject them in the constructor, but place them as a property.
@@ -54,8 +59,8 @@ class UploadImageFragment : Fragment(R.layout.fragment_upload_image){
 
         chooseImageBtn = view.findViewById(R.id.choose_image_btn)
         captureImageBtn = view.findViewById(R.id.capture_image_btn)
-        photoView = view.findViewById(R.id.image_view)
-        //cropFragmentBtn = view.findViewById(R.id.crop_image_button)
+        cropImageView = view.findViewById(R.id.image_view)
+
 
         //This btn is used for instantiating upload to server
         captureImageBtn.apply {
@@ -63,6 +68,9 @@ class UploadImageFragment : Fragment(R.layout.fragment_upload_image){
                 if ( !wasInit { selectedImage } ) {
                     Toast.makeText(this.context, "Select Image First", Toast.LENGTH_SHORT).show()
                 } else {
+
+                    //cropping image
+                     cropImage(selectedImage)
                     Log.i(TAG, "Wait for it...")
                     val file = File(requireActivity().cacheDir, selectedImage.photoFileName)
                     viewModel.onUpload(selectedImage, file)
@@ -70,17 +78,21 @@ class UploadImageFragment : Fragment(R.layout.fragment_upload_image){
             }
         }
 
+        cropImageView.setOnClickListener{
+            //starting intent to get image
+            val intent = Intent()
+            intent.type = "/image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startForResult.launch(intent)
+        }
+
+
         chooseImageBtn.apply {
             setOnClickListener {
                 observer.selectImage()
             }
         }
-        /*
-        cropFragmentBtn.setOnClickListener{
-            val cropFragment = CropFragment()
-            val transaction: FragmentTransaction = FragmentManager.beginTransaction() ?:
-            transaction.replace(R.id.fragment_container, cropFragment)
-        }*/
+
 
         return view
     }
@@ -94,7 +106,7 @@ class UploadImageFragment : Fragment(R.layout.fragment_upload_image){
         val file = File(requireActivity().cacheDir, selectedImage.photoFileName)
         createFileFromBitmap(selectedImage.bitmap, file)
 
-        photoView.setImageBitmap(selectedImage.bitmap)
+        cropImageView.setImageBitmap(selectedImage.bitmap)
     }
 
     //We used an observer to choose image from gallery
@@ -113,17 +125,32 @@ class UploadImageFragment : Fragment(R.layout.fragment_upload_image){
         Log.i(TAG, "Now in start")
     }
 
+    private var startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result: ActivityResult ->
+
+
+        if(result.resultCode == Activity.RESULT_OK) {
+            val resultString = result.data?.data.toString()
+
+            Log.i(TAG,resultString)
+            val image: Bitmap =
+                getBitmap(requireContext(), null, resultString, ::uriToBitmap)
+            //cropped image under results of crop
+            selectedImage.bitmap = image
+
+        }
+    }
+
+    private fun cropImage(selectedImage: UploadedImage) {
+        val croppedImage = cropImageView.croppedImage
+        selectedImage.bitmap = croppedImage
+
+    }
+
     companion object {
         fun newInstance() = UploadImageFragment()
     }
 
-    /*
-    fun switchCropFragment() {
-        //fragmentManager1 = supportFragmentManager
-        fragmentManager1.beginTransaction()
-            .replace(R.id.fragment_container,CropFragment(),"CropFragment").commit()
-    }
-*/
+
 
 
 }
