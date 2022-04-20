c32ba9ebae8c1441855c77d80abd380d3d71dcc8
Thomas Nemeth <thonem10@gmail.com>
2022-04-21 01:19:26 +0200

    trying to make a recycler but not as good as Izzy's, that blows dude

--- app/src/main/java/no/kristiania/android/reverseimagesearchapp/data/local/entity/CollectionRecyclerItem.kt
+++ app/src/main/java/no/kristiania/android/reverseimagesearchapp/data/local/entity/CollectionRecyclerItem.kt
@@ -1,18 +1,18 @@
 package no.kristiania.android.reverseimagesearchapp.data.local.entity
 
 import android.graphics.Bitmap
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.collections.ArrayList
 
 data class CollectionRecyclerItem(
     var urlOnServer: String? = null,
     var collectionName: String? = null,
-    var date: Date? = null,
-    var bitmap: Bitmap? = null
+    var date: String,
+    var image: Int
 )
-
- public fun dummyCollection(): ArrayList<CollectionRecyclerItem>{
-     val list : List<CollectionRecyclerItem>
-     val CollectionRecyclerItem
-
- }
--- app/src/main/java/no/kristiania/android/reverseimagesearchapp/presentation/fragment/DisplayCollectionRecyclerView.kt
+++ app/src/main/java/no/kristiania/android/reverseimagesearchapp/presentation/fragment/DisplayCollectionRecyclerView.kt
@@ -1,29 +1,25 @@
 package no.kristiania.android.reverseimagesearchapp.presentation.fragment
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import no.kristiania.android.reverseimagesearchapp.R
+import no.kristiania.android.reverseimagesearchapp.data.local.entity.CollectionRecyclerItem
+import java.time.LocalDate
+import java.time.LocalDateTime
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DisplayCollectionRecyclerView.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DisplayCollectionRecyclerView : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    val list = mutableListOf<CollectionRecyclerItem>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        dummyCollection()
+
 
     }
 
@@ -39,23 +35,20 @@ class DisplayCollectionRecyclerView : Fragment() {
         )
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DisplayCollectionRecyclerView.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DisplayCollectionRecyclerView().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    fun dummyCollection(){
+        for(i in 0..10){
+            val lol = CollectionRecyclerItem("ur${i}srs", "na${i}me","time is $i",R.drawable.ic_logo)
+            list.add(lol)
+        }
     }
+
+    /*
+    data class CollectionRecyclerItem(
+    var urlOnServer: String? = null,
+    var collectionName: String? = null,
+    var date: LocalDate,
+    var bitmap: Bitmap? = null
+)
+     */
 }
