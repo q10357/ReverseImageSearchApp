6a8d0b88b854d5fe4b6c0e0d672d58b3347da98e
Thomas Nemeth <thonem10@gmail.com>
2022-04-23 00:27:28 +0200

    added fucntion to show image of the clicked

--- app/src/main/java/no/kristiania/android/reverseimagesearchapp/presentation/fragment/DisplayResultFragment.kt
+++ app/src/main/java/no/kristiania/android/reverseimagesearchapp/presentation/fragment/DisplayResultFragment.kt
@@ -13,6 +13,7 @@ import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
